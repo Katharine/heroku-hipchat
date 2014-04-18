badf601f1ba824d92277178b92a4d9e27c4cf20d
Katharine Berry <katharine@getpebble.com>
2014-04-18 02:35:55 -0700

    Now with actual working.

--- webhook.py
+++ webhook.py
@@ -1,21 +1,22 @@
 from os import environ
-from flask import Flask, request, make_response
-import urllib2
+from flask import Flask, request
+import requests
+import json
 
 app = Flask(__name__)
 
 @app.route('/deployed', methods=['POST'])
 def hook():
+    form = {x : request.form[x] for x in request.form}
     to_send = {
         'color': 'purple',
-        'message': "{user} deployed <a href='{url}'>{app}</a> ({head}):<br>{git_log}".format(request.form),
+        'message': "{user} deployed <a href='{url}'>{app}</a> ({head}):<br>{git_log}".format(**form),
         'message_format': 'html'
     }
-    req = urllib2.urlopen("https://api.hipchat.com/v2/room/{HIPCHAT_ROOM}/notification?auth_token={HIPCHAT_TOKEN}".format(environ))
-    try:
-        req.read()
-    finally:
-        req.close()
+    url = "https://api.hipchat.com/v2/room/{HIPCHAT_ROOM}/notification?auth_token={HIPCHAT_TOKEN}".format(**environ)
+    requests.post(url, json.dumps(to_send), headers={'Content-Type': 'application/json'})
+
+    return ''
 
 if __name__ == '__main__':
     app.debug = 'DEBUG' in environ

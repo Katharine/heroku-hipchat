960a502560c58182cc9eefa9d18624213548e889
Katharine Berry <katharine@getpebble.com>
2014-04-18 02:38:54 -0700

    Extra spaces are lame.

--- webhook.py
+++ webhook.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/deployed', methods=['POST'])
 def hook():
-    form = {x : request.form[x] for x in request.form}
+    form = {x: request.form[x] for x in request.form}
     to_send = {
         'color': 'purple',
         'message': "{user} deployed <a href='{url}'>{app}</a> ({head}):<br>{git_log}".format(**form),
